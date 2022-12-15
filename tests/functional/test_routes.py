1a09ac441d7c28100eb3d394fd2ac68abbc42cf1
martagdvalle <martagdvalle@outlook.es>
2022-12-15 16:58:15 +0100

    SECOND

--- tests/functional/test_routes.py
+++ tests/functional/test_routes.py
@@ -29,4 +29,13 @@ def test_create_account(testing_client):
     response = testing_client.post('/accounts', json={'name': 'John Doe', 'currency': '€'})
     assert response.status_code == 200
 
+def test_get_account(testing_client):
+    """
+    GIVEN a Flask application
+    WHEN the '/accounts/<int:id>' page is requested (GET)
+    THEN check the response is valid
+    """
+    response = testing_client.get('/accounts/1')
+    assert response.status_code == 200
+
 
